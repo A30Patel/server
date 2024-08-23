d32c92f0076d99b71ecead654e8518f3c69840d4
Atish Patel <85749961+A30Patel@users.noreply.github.com>
2024-08-23 11:55:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const jsonServer = require('json-server');
 const path = require('path');
+
 const server = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, 'src', 'jobs.json'));
 const middlewares = jsonServer.defaults();
@@ -8,6 +9,7 @@ server.use(middlewares);
 server.use(jsonServer.bodyParser);
 server.use(router);
 
+// Listen on the port provided by Render or default to 8000
 server.listen(process.env.PORT || 8000, () => {
   console.log('JSON Server is running');
 });
